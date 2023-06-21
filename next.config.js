10d14db28be864ceeff616d745210bb4cd3e654b
Erick Otenyo <otenyo.erick@gmail.com>
2023-06-21 15:43:03 +0300

    testing path

--- next.config.js
+++ next.config.js
@@ -40,7 +40,8 @@ const nextConfig = {
   },
   swcMinify: true,
   trailingSlash: true,
-  basePath: process.env.BASE_PATH ? process.env.BASE_PATH : "",
+  basePath: "/cms",
+  assetPrefix: "/cms",
   env: {
     DEBUG: process.env.DEBUG,
     FEATURE_ENV: process.env.FEATURE_ENV,
